eb624b0f480b4693a5052cfa20ed3dbb04fa332b
Risshi25 <risshirajsen@gmail.com>
2025-05-17 16:16:18 +0530

    feat:sidebar complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,7 @@ export default function RootLayout({
         >
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
@@ -42,9 +43,9 @@ export default function RootLayout({
               <main className="py-8">
                 {/* container to center the content */}
                 <div className="max-w-7xl mx-auto px-4">
-                  <div className="*grid grid-cols-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg:col-span-3">sidebar</div>
-                    <div className="lg:col-span-9">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    <div className="hidden lg:block lg:col-span-3 h-fit"><Sidebar/></div>
+                    <div className="col-span-1 lg:col-span-9">
                       {/* main content */}
                       {children}
                     </div>
