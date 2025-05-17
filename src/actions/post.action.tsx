cbfc7e2dc814f756994f85acdf5348d9c1d5ed99
Risshi25 <risshirajsen@gmail.com>
2025-05-18 03:46:31 +0530

    feat: implement post fetching logic

--- src/actions/post.action.tsx
+++ src/actions/post.action.tsx
@@ -1,25 +1,74 @@
-'use server';
+"use server";
 
-import { currentUser } from "@clerk/nextjs/server";
 import { getDbUserId } from "./user.action";
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
-export async function createPost (content:string, image:string){
-    try {
-        const userId = await getDbUserId();
-        const post = await prisma.post.create({
-            data: {
-                content,
-                image,
-                authorId: userId || "",
+export async function createPost(content: string, image: string) {
+  try {
+    const userId = await getDbUserId();
+    const post = await prisma.post.create({
+      data: {
+        content,
+        image,
+        authorId: userId || "",
+      },
+    });
+    revalidatePath("/");
+    return {
+      sucess: true,
+      post,
+    };
+  } catch (err) {
+    return { sucess: false, error: "Failed to create post" };
+  }
+}
+
+export async function getPosts() {
+  try {
+    const posts = await prisma.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        author: {
+          select: {
+            name: true,
+            image: true,
+            username: true,
+          },
+        },
+        comments: {
+          include: {
+            author: {
+              select: {
+                id: true,
+                username: true,
+                image:true,
+                name:true
+              },
+            },
+          },
+          orderBy : {
+            createdAt : "asc"
+          }
+        },
+        likes: {
+            select: {
+                userId: true
             }
-        }) 
-        revalidatePath('/');
-        return {
-            sucess:true,post
+        },
+        _count : {
+            select : {
+                likes: true,
+                comments : true,
+            }
         }
-    } catch(err){
-        return {sucess:false, error:"Failed to create post"}
-    }
+      },
+    });
+    return posts;
+  } catch (error) {
+    console.log("error in get posts",error);
+     throw new Error('Failed to fetch posts')
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,22 @@ import prisma from "@/lib/prisma"
 import CreatePost from '@/components/CreatePost';
 import { currentUser } from "@clerk/nextjs/server";
 import WhoToFollow from "@/components/SuggestedAccounts";
+import {getPosts} from "@/actions/post.action"
+import PostCard from "@/components/PostCard";
 
 export default async function Home() {
   const user = await currentUser();
+  const posts = await getPosts();
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
         {user ? <CreatePost /> : null}
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
       </div>
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
         <WhoToFollow />
