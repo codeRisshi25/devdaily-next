c0e35975a8af730edd88e32dbaa4006d277e8df7
Risshi25 <risshirajsen@gmail.com>
2025-05-17 12:47:20 +0530

    feat:added navbar components for desktop and mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
 import Image from "next/image";
 import { SignInButton, SignOutButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from '../components/ui/button';
+import { ModeToggle } from "@/components/ui/modetoggle";
+import { Sheet } from "@/components/ui/sheet";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 --font-geist-mono">
-      <SignedIn>
-        <h1>Welcome to Dev Daily !</h1>
-        <UserButton />
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <h1>Please sign in</h1>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
+    <div className="m-1.5">
+      <h1>
+        home page content
+      </h1>
     </div>
   );
 }
